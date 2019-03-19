e185f05d48125e005165a6d589379e839b5f2b9b
mikapower <35053@ufp.edu.pt>
2019-03-19 17:22:15 +0000

    updates

--- lib/src/blocs/movies_bloc.dart
+++ lib/src/blocs/movies_bloc.dart
@@ -6,13 +6,23 @@ class MoviesBloc {
   final _repository = Repository();
   final _moviesFetcher = PublishSubject<ItemModel>();
 
+
+
+  //Stream com todos os filmes
   Observable<ItemModel> get allMovies => _moviesFetcher.stream;
 
   fetchAllMovies() async {
+
+      //vai buscar filmes todos os filmes
     ItemModel itemModel = await _repository.fetchAllMovies();
+
+
+    //Adiciona filmes ao stream
     _moviesFetcher.sink.add(itemModel);
   }
 
+
+  //fecha stream
   dispose() {
     _moviesFetcher.close();
   }
